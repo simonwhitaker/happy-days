b892a580dbf57eacc13ccc9e6c2f2a9eef58da22
Simon Whitaker <sw@netcetera.org>
2014-02-16 10:24:19 +0000

    Pop to root if opening already running app in response to notification

--- Happy Days/HDAppDelegate.m
+++ Happy Days/HDAppDelegate.m
@@ -15,6 +15,7 @@
 @property (nonatomic) HDDataController *dataController;
 @property (nonatomic) HDNotificationController *notificationController;
 @property (nonatomic) HDDayViewController *rootDayViewController;
+@property (nonatomic) UINavigationController *navigationController;
 @end
 
 @implementation HDAppDelegate
@@ -36,19 +37,16 @@
     UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:dayViewController];
     
     self.window.rootViewController = navigationController;
+    self.navigationController = navigationController;
     
     [self.window makeKeyAndVisible];
     return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+- (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification {
+    if (application.applicationState != UIApplicationStateActive) {
+        [self.navigationController popToRootViewControllerAnimated:NO];
+    }
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
@@ -59,14 +57,6 @@
 - (void)applicationSignificantTimeChange:(UIApplication *)application {
     // TODO: on significant time change we should check to see if the view is foremost; if so, show a dialog asking the user if they want to switch to the new date
     self.rootDayViewController.date = [NSDate date];
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
 @end
